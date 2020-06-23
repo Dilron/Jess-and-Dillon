0839eb3c6a7c08d2756661a6412c996c34c00105
Dillon O'Kelley <dillonokelley@Dillons-MacBook-Air.local>
2020-06-23 14:05:21 -0600

    ready for first run build

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -7,20 +7,26 @@ export default class Home extends Component {
                 <div className='home-splash mb-seperator' >
                     <section className='home-splash-box' >
                         <h1>We're Getting Married!</h1>
-                        <h2>August 1, 2020</h2>
-                        <div>
-                            <h2>Soldier Hallow</h2>
-                            <h2>Midway, UT</h2>
-                        </div>
-                        <h3>We are so excited to celebrate our wedding day with you on August 1st!</h3>
-                        <button>RSVP</button>
+                        <h2>August 7, 2021</h2>
+                        <h2>Soldier Hallow | Midway, UT</h2>
+                        <h3>We are so excited to celebrate our marriage with you on August 7th next year! Although we will still be married on August 1st, 2020, we are rescheduling our wedding events to 2021 due to COVID. Please visit this website in the coming months to RSVP & stay in loop on our wedding celebrations!
+                            For those invited to our ceremony this year, please find the RSVP link below!
+                        </h3>
+                        {/* <button>RSVP</button> */}
                     </section>
                 </div>
                 <div className='home-info mb-seperator' >
                     <img className='info-banner' src='https://i.imgur.com/38ppLZd.jpg' />
-                    <section className='info-content' ></section>
+                    <section className='info-content' >
+                        <h1>*WEBSITE UNDER CONSTRUCTION, MORE INFO COMING SOON.*</h1>
+                        <h2>For those invited to our ceremony this year, RSVP <a href="https://forms.gle/xPvhjDz4QSqdnjGx6" target="_blank" rel="noopener noreferrer">here</a> for August 1, 2020</h2>
+                        <ul>
+                            <li>Due to COVID, we are rescheduling our wedding events to 2021.</li>
+                            <li>Watch for your Change the Date in the mail & let us know if don't receive one.</li>
+                        </ul>
+                    </section>
                 </div>
-                <img className='content-splash mb-seperator' src='https://i.imgur.com/JQg89Vx.jpg' />
+                <img className='content-location mb-seperator' src='https://i.imgur.com/eH1uAyT.jpg' />
                 <img className='content-splash mb-seperator' src='https://i.imgur.com/O0xyQsE.jpg' />
             </div>
         )
--- src/components/Nav/contact.jsx
+++ src/components/Nav/contact.jsx
@@ -5,16 +5,17 @@ export default class Contact extends Component {
         return (
             <div className="contact-container" >
                 <h1>Contact Us</h1>
-                    <div className='contact-info-row' >
+                <a href="mailto:jessanddillon2020@gmail.com" target="_blank" rel="noopener noreferrer" >jessanddillon2020@gmail.com</a>
+                <div className='contact-info-row' >
                     <h2>Dillon</h2>
-                        <h2>Phone: 541-728-4059</h2>
-                        <h2>Email: dillonokelle@gmail.com</h2>
-                    </div>
-                    <div className='contact-info-row'>
+                    <h2>Phone: 541-728-4059</h2>
+                    <h2>Email: <a href="mailto:dillonokelle@gmail.com" target="_blank" rel="noopener noreferrer" >dillonokelle@gmail.com</a> </h2>
+                </div>
+                <div className='contact-info-row'>
                     <h2>Jess</h2>
-                        <h2>Phone: 801-663-9496</h2>
-                        <h2>Email: jess.esplin@gmail.com</h2>
-                    </div>
+                    <h2>Phone: 801-663-9496</h2>
+                    <h2>Email: <a href="mailto:jess.esplin@gmail.com" target="_blank" rel="noopener noreferrer" >jess.esplin@gmail.com</a> </h2>
+                </div>
             </div>
         )
     }
