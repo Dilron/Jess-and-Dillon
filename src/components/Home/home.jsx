9936d40939396dc3597459974b844ecb75a332e5
Dillon O'Kelley <dillonokelley@Dillons-MacBook-Air.local>
2020-06-24 23:53:27 -0600

    styling changes

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -9,7 +9,7 @@ export default class Home extends Component {
                         <h1>We're Getting Married!</h1>
                         <h2>August 7, 2021</h2>
                         <h2>Soldier Hollow | Midway, UT</h2>
-                        <h3>We are so excited to celebrate our marriage with you on August 7th next year! Although we will still be married on August 1st, 2020, we are rescheduling our wedding events to 2021 due to COVID. Please visit this website in the coming months to RSVP & stay in loop on our wedding celebrations!
+                        <h3>We are so excited to celebrate our marriage with you on August 7th next year! Although we will still be married on August 1st, 2020, we are rescheduling our wedding events to 2021 due to COVID. Please visit this website in the coming months to RSVP & stay in the loop on our wedding celebrations!
                             For those invited to our ceremony this year, please find the RSVP link below!
                         </h3>
                         {/* <button>RSVP</button> */}
