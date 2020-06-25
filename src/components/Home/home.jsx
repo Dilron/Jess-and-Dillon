5f5c8748b70cea8ea2951c282ee08a0051c5c477
Dillon O'Kelley <dillonokelley@Dillons-MacBook-Air.local>
2020-06-24 23:42:13 -0600

    style changes

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -8,7 +8,7 @@ export default class Home extends Component {
                     <section className='home-splash-box' >
                         <h1>We're Getting Married!</h1>
                         <h2>August 7, 2021</h2>
-                        <h2>Soldier Hallow | Midway, UT</h2>
+                        <h2>Soldier Hollow | Midway, UT</h2>
                         <h3>We are so excited to celebrate our marriage with you on August 7th next year! Although we will still be married on August 1st, 2020, we are rescheduling our wedding events to 2021 due to COVID. Please visit this website in the coming months to RSVP & stay in loop on our wedding celebrations!
                             For those invited to our ceremony this year, please find the RSVP link below!
                         </h3>
@@ -19,8 +19,9 @@ export default class Home extends Component {
                     <img className='info-banner' src='https://i.imgur.com/38ppLZd.jpg' />
                     <section className='info-content' >
                         <h1>*WEBSITE UNDER CONSTRUCTION, MORE INFO COMING SOON.*</h1>
-                        <h2>For those invited to our ceremony this year, RSVP <a href="https://forms.gle/xPvhjDz4QSqdnjGx6" target="_blank" rel="noopener noreferrer">here</a> for August 1, 2020</h2>
+                        <h2><b>For those invited to our ceremony this year, RSVP <a href="https://forms.gle/xPvhjDz4QSqdnjGx6" target="_blank" rel="noopener noreferrer">here</a> for August 1, 2020</b></h2>
                         <ul>
+                            <li>Directions for the August 1st, 2020 ceremony coming soon</li>
                             <li>Due to COVID, we are rescheduling our wedding events to 2021.</li>
                             <li>Watch for your Change the Date in the mail & let us know if don't receive one.</li>
                         </ul>
